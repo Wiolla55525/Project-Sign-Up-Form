f3cb5a6b0dd2b078089c90f8d293c46737bcf470
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-09-28 20:44:23 +0300

    style: :art: input style change

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const error = document.getElementById("error");
 function crosscheckPasswords(e) {
   const message = [];
   if (password.value !== confirmPassword.value) {
-    message.push("Passwords do not match!");
+    message.push("* Passwords do not match!");
   }
   if (message.length > 0) {
     e.preventDefault();
